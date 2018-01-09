bd6de06751abd3cac515918928f013fe3965e1bf
jigsaw <m@jgs.me>
2018-01-09 15:05:16 +0900

    :zap:

--- index.js
+++ index.js
@@ -2,10 +2,39 @@ import './style'
 import {Component} from 'preact'
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      color: this.generateColor(),
+    }
+  }
+
+  generateColor() {
+    return `#${this.generateHex()}${this.generateHex()}${this.generateHex()}`
+  }
+
+  generateHex() {
+    const candidate = Math.floor(Math.random() * 256).toString(16)
+    return candidate.length === 1 ? `0${candidate}` : candidate
+  }
+
   render() {
+    const style = {
+      backgroundColor: this.state.color,
+    }
     return (
-      <div>
-        <h1>Hello, World!</h1>
+      <div style={style}>
+        <h1>
+          <span>{this.state.color}</span>
+        </h1>
+        <div>
+          <span>
+            <span>&copy;&nbsp;2018&nbsp;Takaya&nbsp;Kobayashi&nbsp;</span>
+            <a href="https://kbys.tk/y" target="_blank">
+              https://kbys.tk/y
+            </a>
+          </span>
+        </div>
       </div>
     )
   }
